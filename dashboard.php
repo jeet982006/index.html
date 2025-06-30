b4ee080a4472d9b61234f594635cf3d5b248fdc0
jeet982006 <merjeet98@gmail.com>
2025-06-30 19:49:10 +0530

    dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.html");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head><title>Dashboard</title></head>
+<body>
+    <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
